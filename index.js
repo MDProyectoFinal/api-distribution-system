774d470f18ebd3acb4cceaff661879fdce0fe6fe
Matias Clement <proyectofinalisiutn@gmail.com>
2023-12-12 21:04:18 -0300

    master - agregado de .env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 3977;
+var port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
 mongoose.set('strictQuery', false);
@@ -11,12 +11,12 @@ mongoose.set('strictQuery', false);
 //mongoose.connect('mongodb://0.0.0.0:27017/rosario_snack').then( 
 
 // Conectar Mongo DB Atlas Free Online
-mongoose.connect('mongodb+srv://proyectofinalisiutn:ProyectoFinalIsiUtn2023@clusterbd.q87qmbk.mongodb.net/rosario_snack').then( 
+mongoose.connect(process.env.MONGO_URL).then( 
     res => {
         console.log(`La conexión a la base de datos está funcionando correctamente..`);  
         
         app.listen(port, function(){
-            console.log('Servidor del api rest de proyecto final escuchando en http://localhost:' + port );
+            console.log('Servidor del api rest de proyecto final escuchando en ' + process.env.URL_ACTUAL + ':' + process.env.PORT );
         })
     },
     err => console.error(`Ha habido un error: ${err}`),    
