66558d5273a1cda7233e969677a214a2283a9d9f
Leonel Villa <leovilla91@gmail.com>
2024-04-17 09:12:20 -0300

    Agregar schema item pedido

--- src/modelos/pedido.ts
+++ src/modelos/pedido.ts
@@ -1,25 +1,21 @@
 'use strict'
 
-import mongoose from "mongoose";
-import { Schema } from "mongoose";
+import mongoose from 'mongoose'
+import { Schema } from 'mongoose'
+
+let PedidoItemSchema = new mongoose.Schema({
+  idProducto: { type: Schema.ObjectId, required: true, ref: 'Producto' },
+  cantidad: { type: Number, required: true, min: [1, 'La cantidad no puede ser menor a 1'] },
+  precio: { type: Number, required: true }, // No estoy seguro guardarlo en este punto u obtenerlo de un histórico de precioes
+  total: { type: Number, required: true }, // No estoy seguro de guardar esto porque es un atributo calculado
+})
 
 const pedidoSchema = new mongoose.Schema({
-    fecha_alta_pedido: { type: Date, required: true, default: new Date() },
-    estado: { type: String, required: true },
-    cliente: { type: Schema.ObjectId, required: true, ref: 'Cliente' },
-    producto: { type: Schema.ObjectId, required: true, ref: 'Producto' }
-});
+  fecha_alta_pedido: { type: Date, required: true, default: new Date() },
+  estado: { type: String, required: true },
+  cliente: { type: Schema.ObjectId, required: true, ref: 'Cliente' },
+  items: [PedidoItemSchema],
+  subtotal: { type: Number, default: 0 },
+})
 
-export const PedidoModel = mongoose.model('Pedido', pedidoSchema);
-
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-
-// var PedidoSchema = Schema({
-//     fecha_alta_pedido: Date,
-//     estado: String,
-//     cliente: { type: Schema.ObjectId, ref: 'Cliente' },
-//     producto: { type: Schema.ObjectId, ref: 'Producto' }
-// })
-
-// module.exports = mongoose.model('Pedido', PedidoSchema);
+export const PedidoModel = mongoose.model('Pedido', pedidoSchema)
