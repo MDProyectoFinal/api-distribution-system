0aa5d0cd150f1781ee53a8161d301dc7c048ed5e
Matias Clement <proyectofinalisiutn@gmail.com>
2024-04-27 10:05:10 -0300

    Se acomoda modelo usuario y se añade actualizacion masiva de baja y fecha_baja

--- src/modelos/usuario.ts
+++ src/modelos/usuario.ts
@@ -3,27 +3,32 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
 
 export interface IUsuario extends Document {
-    _id: typeof Schema.ObjectId; // Ver si BORRARLO. OJO!!
-    persona: String, // { type: Schema.ObjectId, ref: 'Persona' },
-    nombre_usuario: String,
-    clave: String,
-    email: String,
-    rol: String, // Sería el Tipo de Usuario
-    imagen: String,
-    fecha_registro: Date,
-    fecha_ultimo_inicio_sesion: Date
+    //_id: typeof Schema.ObjectId; // Ver si BORRARLO. OJO!!
+    persona: String | null, // { type: Schema.ObjectId, ref: 'Persona' },
+    nombre_usuario: String | null,
+    clave: String | null,
+    email: String | null,
+    rol: String | null, // Sería el Tipo de Usuario
+    imagen: String | null,
+    fecha_registro: Date | null,
+    fecha_ultimo_inicio_sesion: Date | null,
+    baja: boolean | null,
+    fecha_baja: Date | null,
 }
 
-const UsuarioSchema: Schema = new Schema<IUsuario>({
-
+//const UsuarioSchema: Schema = new Schema<IUsuario>({
+// const UsuarioSchema: IUsuario = new Schema({
 const UsuarioSchema: Schema = new Schema({
-    persona: { type: String, require: true }, // { type: Schema.ObjectId, ref: 'Persona' },
+    _id: { type: mongoose.Types.ObjectId, auto: true },
+    persona: { type: mongoose.Types.ObjectId, require: true }, // { type: Schema.ObjectId, ref: 'Persona' },
     nombre_usuario: { type: String, require: true },
     clave: { type: String, require: true },
     email: { type: String, require: true },
     rol: { type: String, enum: ['ADMIN','USER'], default: 'USER' }, // OJO!!! Ver si funciona
     imagen: { type: String, require: true },
-    fecha_registro: { type: Date, default: new Date() }, // Ver si funciona
-    fecha_ultimo_inicio_sesion: { type: Date, default: new Date() } // OJO!!! Ver si funciona
+    fecha_registro: { type: Date, default: Date.now }, // Ver si funciona
+    fecha_ultimo_inicio_sesion: { type: Date, default: Date.now }, // OJO!!! Ver si funciona
+    baja: { type: Boolean, default: false },
+    fecha_baja: { type: Date, default: null }
 })
 export const UsuarioModel = mongoose.model<IUsuario>('Usuario', UsuarioSchema);
