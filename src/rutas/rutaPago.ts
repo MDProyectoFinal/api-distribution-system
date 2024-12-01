4d09662ddbf9dd5f14d386530376cb8be34d5909
Matias Clement <proyectofinalisiutn@gmail.com>
2024-12-01 20:21:57 -0300

    borrado de código innecesario

--- src/rutas/rutaPago.ts
+++ src/rutas/rutaPago.ts
@@ -4,14 +4,3 @@ import { crearPreferenciaPago } from '../controladores/controladorPago';
 export const router = express.Router()
 
 router.post('/', crearPreferenciaPago);
-
-// router.get('/', recuperarTodos)
-// router.post('/filtrar', recuperarPorFiltros)
-// router.get('/:idPedido', recuperarPorId)
-// //router.post('/', validarExisteUsuarioBody, insertarPedido)
-// router.post('/', insertarPedido)
-// //router.put('/:idPedido', validarExisteUsuarioBody, actualizacionCompleta)
-// router.put('/:idPedido', actualizacionCompleta)
-// router.patch('/:idPedido', actualizacionParcial)
-// router.delete('/:idPedido', eliminarPorId)
-// router.put('/cambiarEstadoPedido/:idPedido', cambiarEstadoPorIdPedido)
