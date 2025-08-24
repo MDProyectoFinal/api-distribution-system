ab1e72306072097519af2d3c0b76e35ec508f7a2
Leonel Villa <leovilla91@gmail.com>
2025-08-24 14:17:38 -0300

    Quitar de jwt las propiedades no utilizadas en el frontend

--- src/servicios/jwt.ts
+++ src/servicios/jwt.ts
@@ -8,17 +8,14 @@ import { IUsuario } from '../modelos/usuario';
 
 // Recibe un usuario y genera un token
 exports.createToken = function( usuario: IUsuario ){
-    
+
     var payload = {
         sub: usuario._id, // id del documento en BD. Ver que hacer acá!
         persona: usuario.persona,
         nombre_usuario: usuario.nombre_usuario,
         email: usuario.email,
-        clave: usuario.clave,
         rol: usuario.rol,
         imagen: usuario.imagen,
-        fecha_registro: usuario.fecha_registro,
-        fecha_ultimo_inicio_sesion: usuario.fecha_ultimo_inicio_sesion,
         iat: moment().unix(), // Fecha inicial/creacion -> Tiempo actual en formato timestamp
         exp: moment().add(30, 'days').unix() // Fecha de expiracion
     }
